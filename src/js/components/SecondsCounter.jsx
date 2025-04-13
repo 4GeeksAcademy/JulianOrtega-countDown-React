e3439fccc3a0d2057d4d4f45e57f1851eb5545ec
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-04-13 21:17:44 +0000

    Cambio pedido por NataLyyy

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,63 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-class SecondsCounter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      seconds: props.start || 0,
-      isRunning: true,
-    };
-    this.interval = null;
-  }
+const SecondsCounter = ({ start = 0, alertTime }) => {
+  const [seconds, setSeconds] = useState(start);
+  const [isRunning, setIsRunning] = useState(true);
 
-  componentDidMount() {
-    this.startCounter();
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.alertTime && this.state.seconds === this.props.alertTime) {
-      alert(`Tiempo alcanzado: ${this.props.alertTime} segundos`);
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setInterval(() => {
+        setSeconds(prev => prev + 1);
+      }, 1000);
     }
-  }
+    return () => clearInterval(interval);
+  }, [isRunning]);
 
-  startCounter = () => {
-    this.interval = setInterval(() => {
-      if (this.state.isRunning) {
-        this.setState((prevState) => ({ seconds: prevState.seconds + 1 }));
-      }
-    }, 1000);
+  useEffect(() => {
+    if (alertTime && seconds === alertTime) {
+      alert(`Tiempo alcanzado: ${alertTime} segundos`);
+    }
+  }, [seconds, alertTime]);
+
+  const toggleCounter = () => {
+    setIsRunning(prev => !prev);
   };
 
-  toggleCounter = () => {
-    this.setState((prevState) => ({ isRunning: !prevState.isRunning }));
+  const resetCounter = () => {
+    setSeconds(0);
+    setIsRunning(true);
   };
 
-  resetCounter = () => {
-    this.setState({ seconds: 0 });
-  };
-
-  render() {
-    return (
-      <div className="container text-center mt-5">
-        <div className="display-1 bg-dark text-light p-3 rounded">
-          {this.state.seconds}
-        </div>
-        <div className="mt-3">
-          <button className="btn btn-primary me-2" onClick={this.toggleCounter}>
-            {this.state.isRunning ? "Pausar" : "Reanudar"}
-          </button>
-          <button className="btn btn-danger" onClick={this.resetCounter}>
-            Reiniciar
-          </button>
-        </div>
+  return (
+    <div className="container text-center mt-5">
+      <div className="display-1 bg-dark text-light p-3 rounded">
+        {seconds}
       </div>
-    );
-  }
-}
+      <div className="mt-3">
+        <button className="btn btn-primary me-2" onClick={toggleCounter}>
+          {isRunning ? "Pausar" : "Reanudar"}
+        </button>
+        <button className="btn btn-danger" onClick={resetCounter}>
+          Reiniciar
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default SecondsCounter;
